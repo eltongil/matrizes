6f639bd14ce788a0cc8c8d3dd178609275e2bf3b
eltongil <eltongilx@gmail.com>
2019-08-03 20:18:15 -0300

    inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+Tentativa de criar uma biblioteca robusta para trabalhar com matrizes em C.
+Objetiva apenas treinamento.
---
+++ matriz.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct matriz
+{
+	void *ponto;
+	int col;
+	int lin;
+};
+
+void inserir_matriz(struct matriz *A)
+{
+	printf("Inserir a quantidade de linhas: ");
+	scanf("%d",&A->lin);
+	printf("Inserir a quantidade de colunas: ");
+	scanf("%d",&A->col);
+	for(int i=0;i<lin;i++)
+	{
+		printf()
+		for(int =0;j<col;++)
+		{
+			
+		}
+	}
+}
